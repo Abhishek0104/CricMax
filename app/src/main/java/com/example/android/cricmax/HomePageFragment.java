fc99f155b9932870e16f01c34d1f8fbb94fd5703
Abhishek0104 <abhishek.raut.0104@gmail.com>
2017-09-10 21:52:53 +0530

    Live Score Page Added

--- app/src/main/java/com/example/android/cricmax/HomePageFragment.java
+++ app/src/main/java/com/example/android/cricmax/HomePageFragment.java
@@ -1,17 +1,23 @@
 package com.example.android.cricmax;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.android.cricmax.LiveMatches.LiveMatchesActivity;
 
 /**
  * Created by Abhishek on 10-09-2017.
  */
 
 public class HomePageFragment extends Fragment {
+
+    Button mLiveMatchesBtn;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +30,15 @@ public class HomePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_page, container, false);
 
+        mLiveMatchesBtn = view.findViewById(R.id.live_matches_btn);
+
+        mLiveMatchesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LiveMatchesActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/example/android/cricmax/LiveMatches/LiveMatchesFragment.java
+++ app/src/main/java/com/example/android/cricmax/LiveMatches/LiveMatchesFragment.java
@@ -1,10 +1,35 @@
 package com.example.android.cricmax.LiveMatches;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.android.cricmax.R;
 
 /**
  * Created by Abhishek on 10-09-2017.
  */
 
 public class LiveMatchesFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.live_matches, container, false);
+
+
+        return view;
+    }
+
 }
